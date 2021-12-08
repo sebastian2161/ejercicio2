39303159f06dbdc0cb5524f6c7afb7376d4b9db1
sebastian2161 <sebastiancoll25@gmail.com>
2021-12-08 19:37:43 +0000

    work01

--- odoo_academy/controllers/academy_controllers.py
+++ odoo_academy/controllers/academy_controllers.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
 
 from odoo import http
+#from odoo import http
+#import xmlrpc, xmlrpc.client
+from xmlrpc import client
+import json
+
 
 
 class Academy(http.Controller):
@@ -22,3 +27,118 @@ class Academy(http.Controller):
      #def courses1(self, course, **kw):
      #   return self.course(course.id)
      #   })
+    
+    @http.route('/academy/api', auth= 'public', website=True)
+    def index(self, **kw):   
+        sales_orders = http.request.env['sale.order'].search([])
+        output = "<h1>Sales Orders</h1><ul>"
+        
+        for sale in sales_orders:
+            output += '<li>' + sale['name'] + '</li>'
+        output += "</ul>"
+        
+        return output
+    
+
+    
+    # End-Point que busca el id del usuario activo 
+    @http.route('/restfulapi/login', auth='none', website=True)
+    def login(self, **kw):
+        #xmlrpclib = xmlrpc.client
+        
+        url = 'https://sebastian2161-ejercicio21-rama-v12-3748221.dev.odoo.com'
+        db = 'sebastian2161-ejercicio21-rama-v12-3748221'
+        username = 'admin'
+        password = 'admin'
+         
+        common = client.ServerProxy('{}/xmlrpc/2/common'.format(url))
+        #print(common.version())
+        try:
+            uid = common.authenticate(db, username, password, {})
+            return json.dumps({'user_id':uid})
+        except:
+            return json.dumps({'Error':'Invalid Request'})
+        
+    
+    
+    
+    # End-Point que lista todos los productos con sus precios.
+    @http.route('/restfulapi/get-products', auth='none', website=True)
+    def get_products(self, **kw):
+        #xmlrpclib = xmlrpc.client
+        
+        url = 'https://sebastian2161-ejercicio21-rama-v12-3748221.dev.odoo.com'
+        db = 'sebastian2161-ejercicio21-rama-v12-3748221'
+        username = 'admin'
+        password = 'admin'
+    
+        models = client.ServerProxy('{}/xmlrpc/2/object'.format(url))
+        common = client.ServerProxy('{}/xmlrpc/2/common'.format(url))
+        #print(common.version())
+        #return 'Hola Mundo'
+        
+        try:
+            uid = common.authenticate(db, username, password, {})
+            res = models.execute_kw(db, uid, password, 'product.product', 'search_read',
+                                    [[['id', 'in',[15,318,361]]]], 
+                                    {'fields': ['id', 'price', 'price_extra', 'lst_price', 'default_code']})
+            
+            return json.dumps({'products':res})
+        except:
+            return json.dumps({'Error':'Invalid Request'})
+        
+        
+        
+        
+    # End-Point que busca un producto en especifico.    
+    @http.route('/restfulapi/product/<int:var>', auth='none', website=True)
+    def product(self,var, **kw):
+        #xmlrpclib = xmlrpc.client
+        
+        url = 'https://sebastian2161-ejercicio21-rama-v12-3748221.dev.odoo.com'
+        db = 'sebastian2161-ejercicio21-rama-v12-3748221'
+        username = 'admin'
+        password = 'admin'
+    
+        models = client.ServerProxy('{}/xmlrpc/2/object'.format(url))
+        common = client.ServerProxy('{}/xmlrpc/2/common'.format(url))
+        #print(common.version())
+        #return 'Hola Mundo'
+        
+        try:
+            uid = common.authenticate(db, username, password, {})
+            res = models.execute_kw(db, uid, password, 'product.product', 'search_read',
+                                    [[['id', '=', int(var)]]], 
+                                    {'fields': ['id', 'price', 'price_extra', 'lst_price', 'default_code']})
+            
+            return json.dumps({'products':res})
+        except:
+            return json.dumps({'Error':'Invalid Request'})
+        
+        
+        
+        
+    # End-Point crear un producto nuevo    
+    #@http.route('/restfulapi/crear_product/<string:prod>', auth='none', website=True)
+    #def crear_product(self, prod, **kw):
+        #xmlrpclib = xmlrpc.client
+        
+    #    url = 'https://sebastian2161-ejercicio21-rama-v12-3748221.dev.odoo.com'
+    #    db = 'sebastian2161-ejercicio21-rama-v12-3748221'
+    #    username = 'admin'
+    #    password = 'admin'
+    
+    #    models = client.ServerProxy('{}/xmlrpc/2/object'.format(url))
+    #    common = client.ServerProxy('{}/xmlrpc/2/common'.format(url))
+        #print(common.version())
+        #return 'Hola Mundo'
+        
+        #try:
+    #    uid = common.authenticate(db, username, password, {})
+        #id1 = models.execute_kw(db, uid, password, 'product.template', 'create', [{'name': prod,}])
+    #    id2 = models.execute_kw(db, uid, password, 'product.product', 'create', [{'default_code': prod,'categ_id':8}])
+        
+        
+    #    return json.dumps({'products.product':id2, 'mensaje': 'Registro creado'})
+        #except:
+        #    return json.dumps({'Error':'Invalid Request'})
