d4acc884815556a90abde45abc54fad0e6096594
sebastian2161 <sebastiancoll25@gmail.com>
2022-03-09 23:25:39 +0000

    work57

--- odoo_academy/models/__init__.py
+++ odoo_academy/models/__init__.py
@@ -6,3 +6,4 @@ from . import sale_order
 from . import product_template
 from . import pos_payment_method
 from . import pos_payment
+from . import pos_ssesion
--- odoo_academy/models/pos_payment.py
+++ odoo_academy/models/pos_payment.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from odoo.exceptions import UserError, ValidationError
 
 class PosPayment01(models.Model):
     _inherit = 'pos.payment'
@@ -10,11 +11,35 @@ class PosPayment01(models.Model):
     #payment_method_id01 = fields.Many2one(comodel_name='pos.payment.method', string='importe_tipo_metodo', required=True)
     #tipo_pago01 = fields.Many2one(string='tipo_pago01',related='payment_method_id01.importe_tipo_metodo')
     
-    importe_tipo_metodo = fields.Float(string='importe tipo metodo', related='payment_method_id.importe_tipo_metodo')
+    importe_tipo_metodo = fields.Float(string='importe tipo metodo', related='payment_method_id.importe_tipo_metodo', store=True)
     #importe_prueba01 = fields.Monetary(string='importe 01', default=1, required=True,oldname='importe_tipo_metodo', help="Importe 01")
     
     #campo01 = fields.Char(string ='campo01', default='t', required = True)
     #payment_method_pb02 = fields.Many2one('pos.payment.method', string='Payment Method', required=True)
+    
+    #valor = fields.Float(string='valor', compute='_get_last', store=True)
+    
+    
+    #@api.depends('importe_tipo_metodo')
+    #def _get_last(self):
+    #    reg =[]
+        
+    #    for rec in self:
+    #        reg.append(rec.importe_tipo_metodo)
+    #    suma = sum([x.importe_tipo_metodo for x in reg])
+    #    raise ValidationError(suma)
+        
+            
+            
+    #@api.constrains('importe_tipo_metodo')
+    #def _check(self):
+    #    for record in self:
+    #        if self.importe_tipo_metodo > 0.00:
+    #            raise ValidationError(record.importe_tipo_metodo)
+            
+    
+    
+    
     
     
     
