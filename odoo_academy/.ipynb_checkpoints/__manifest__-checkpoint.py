c3fd950980faffd5d03c65e463716857da937696
sebastian2161 <sebastiancoll25@gmail.com>
2022-03-10 19:52:34 -0300

    work01

---
+++ odoo_academy/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Odoo Academy',
+    'summary': """Academy app to manage training""",
+    'description': """
+        Academy module to manage training:
+         -courses
+         -sessions
+         -attendes
+     """,
+    
+    'author': 'seba',
+    'website': 'https://www.odoo.com/',
+    'category': 'Training',
+    'version': '0.1',
+    'depends': ['sale','website_form','website'],
+    'data': [
+        'security/academy_security.xml',
+        'security/ir.model.access.csv',
+        'views/academy_menuitems.xml',
+        'views/course_views.xml',
+        'views/session_views.xml',
+        'views/sale_views_inherit.xml',
+        'views/product_views_inherit.xml',
+        'wizard/sale_wizard_view.xml',
+        'report/session_report_template.xml',
+        'views/prueba_web.xml',
+        'views/academy_course_controllers.xml',
+        'views/todo_templates.xml',
+        'views/todo_extend.xml',
+        'views/todo_web.xml',
+    ],
+    'demo': [
+        'demo/academy_demo.xml',
+    ],
+    
+}
+
+# Manifiesto de hoy 20-09-2021
+# Manifiesto de hoy 25-09-2021
+# Manifiesto de hoy 27-09-2021
+# Manifiesto de hoy 29-09-2021
+# Manifiesto de hoy 29-09-2021
+# Manifiesto de hoy 29-09-2021
+# Manifiesto de hoy 29-09-2021
+# Manifiesto de hoy 05-10-2021
+# Manifiesto de hoy 07-10-2021
+# Manifiesto de hoy 09-10-2021
+# Manifiesto de hoy 11-10-2021
+# Manifiesto de hoy 13-10-2021
+# Manifiesto de hoy 15-10-2021
+# Manifiesto de hoy 17-10-2021
+# Manifiesto de hoy 19-10-2021
+# Manifiesto de hoy 21-10-2021
+# Manifiesto de hoy 21-10-2021
+# Manifiesto de hoy 23-10-2021
+# Manifiesto de hoy 24-10-2021
+# Manifiesto de hoy 25-10-2021
+# Manifiesto de hoy 28-10-2021
+# Manifiesto de hoy 30-10-2021
+# Manifiesto de hoy 30-10-2021
+# Manifiesto de hoy 03-11-2021
+# Manifiesto de hoy 05-11-2021
+# Manifiesto de hoy 11-11-2021
+# Manifiesto de hoy 11-11-2021
+# Manifiesto de hoy 11-11-2021
+# Manifiesto de hoy 12-11-2021
+# Manifiesto de hoy 14-11-2021
+# Manifiesto de hoy 14-11-2021
+# Manifiesto de hoy 22-11-2021
+# Manifiesto de hoy 22-11-2021
+# Manifiesto de hoy 22-11-2021
---
+++ odoo_academy/controllers/academy_controllers.py
@@ -0,0 +1,251 @@
+# -*- coding: utf-8 -*-
+
+from odoo import http
+#from odoo import http
+#import xmlrpc, xmlrpc.client
+from xmlrpc import client
+import re
+import requests
+import werkzeug.urls
+import json
+
+CORS = '*'
+
+
+
+class Academy(http.Controller):
+    @http.route('/academy/', auth='public', website=True)
+    def index(self, **kw):
+        #return "Hello, world"
+        return http.request.render('odoo_academy.prueba_web')
+    
+    @http.route('/academy/courses/<int:var>', auth='public', website=True)
+    def courses(self, var, **kw):
+        #courses = http.request.env['academy.course'].search([])
+        courses = http.request.env['academy.course'].search([('id', '=', int(var))])
+        return http.request.render('odoo_academy.course_website', {
+            'courses': courses,
+        })
+    
+    
+     #   @http.route('/academy/courses1/<model('academy.course'):course>', auth='public', website=True)
+     #def courses1(self, course, **kw):
+     #   return self.course(course.id)
+     #   })
+    
+    @http.route('/academy/api', auth= 'public', website=True)
+    def index(self, **kw):   
+        sales_orders = http.request.env['sale.order'].search([])
+        output = "<h1>Sales Orders</h1><ul>"
+        
+        for sale in sales_orders:
+            output += '<li>' + sale['name'] + '</li>'
+        output += "</ul>"
+        
+        return output
+    
+
+    
+    # End-Point que busca el id del usuario activo 
+    @http.route('/restfulapi/login', auth='none', website=True)
+    def login01(self, **kw):
+        #xmlrpclib = xmlrpc.client
+        
+        url = 'https://sebastian2161-ejercicio21-rama-v12-3904709.dev.odoo.com'
+        db = 'sebastian2161-ejercicio21-rama-v12-3904709'
+        username = 'admin'
+        password = 'admin'
+         
+        common = client.ServerProxy('{}/xmlrpc/2/common'.format(url))
+        #print(common.version())
+        try:
+            uid = common.authenticate(db, username, password, {})
+            return json.dumps({'user_id':uid})
+        except:
+            return json.dumps({'Error':'Invalid Request'})
+        
+    
+    # End-Point que busca el id del usuario activo 
+    @http.route('/restfulapi/login1', type='json', auth='none', cors=CORS)
+    def login02(self, db=None, login=None, password=None, **kw):
+        #xmlrpclib = xmlrpc.client
+        
+        url = 'https://sebastian2161-ejercicio21-rama-v12-3910879.dev.odoo.com'
+        db = db
+        username = login
+        password = password
+        
+        #return db
+         
+        common = client.ServerProxy('{}/xmlrpc/2/common'.format(url))
+        #print(common.version())
+        try:
+            uid = common.authenticate(db, username, password, {})
+            return json.dumps({'user_id':uid})
+        except:
+            return json.dumps({'Error':'Invalid Request'})
+        
+        
+        # End-Point que busca el id del usuario activo 
+    @http.route('/restfulapi/login2', auth='none', cors=CORS)
+    def login03(self, db=None, login=None, password=None, **kw):
+        #xmlrpclib = xmlrpc.client
+        
+        url = 'https://sebastian2161-ejercicio21-rama-v12-3910879.dev.odoo.com'
+        db = db
+        username = login
+        password = password
+        
+        #return db
+         
+        common = client.ServerProxy('{}/xmlrpc/2/common'.format(url))
+        #print(common.version())
+        try:
+            uid = common.authenticate(db, username, password, {})
+            return json.dumps({'user_id':uid})
+        except:
+            return json.dumps({'Error':'Invalid Request'})
+        
+        
+    
+    
+    
+    # End-Point que lista todos los productos con sus precios.
+    @http.route('/restfulapi/get-products', auth='none', website=True)
+    def get_products(self, **kw):
+        #xmlrpclib = xmlrpc.client
+        
+        url = 'https://sebastian2161-ejercicio21-rama-v12-3910879.dev.odoo.com'
+        db = 'sebastian2161-ejercicio21-rama-v12-3910879'
+        username = 'admin'
+        password = 'admin'
+    
+        models = client.ServerProxy('{}/xmlrpc/2/object'.format(url))
+        common = client.ServerProxy('{}/xmlrpc/2/common'.format(url))
+        #print(common.version())
+        #return 'Hola Mundo'
+        
+        try:
+            uid = common.authenticate(db, username, password, {})
+            res = models.execute_kw(db, uid, password, 'product.product', 'search_read',
+                                    [[['id', 'in',[15,318,361]]]], 
+                                    {'fields': ['id', 'price', 'price_extra', 'lst_price', 'default_code']})
+            
+            return json.dumps({'products':res})
+        except:
+            return json.dumps({'Error':'Invalid Request'})
+        
+        
+        
+        
+    # End-Point que busca un producto en especifico.    
+    @http.route('/restfulapi/product/<int:var>', auth='none', website=True)
+    def product(self,var, **kw):
+        #xmlrpclib = xmlrpc.client
+        
+        url = 'https://sebastian2161-ejercicio21-rama-v12-3748221.dev.odoo.com'
+        db = 'sebastian2161-ejercicio21-rama-v12-3748221'
+        username = 'admin'
+        password = 'admin'
+    
+        models = client.ServerProxy('{}/xmlrpc/2/object'.format(url))
+        common = client.ServerProxy('{}/xmlrpc/2/common'.format(url))
+        #print(common.version())
+        #return 'Hola Mundo'
+        
+        try:
+            uid = common.authenticate(db, username, password, {})
+            res = models.execute_kw(db, uid, password, 'product.product', 'search_read',
+                                    [[['id', '=', int(var)]]], 
+                                    {'fields': ['id', 'price', 'price_extra', 'lst_price', 'default_code']})
+            
+            return json.dumps({'products':res})
+        except:
+            return json.dumps({'Error':'Invalid Request'})
+        
+
+    @http.route('/restfulapi/api_externa', auth='none', website=True)
+    def api_externa(self, **kw):
+        
+        url = 'https://jsonplaceholder.typicode.com/users/'
+        
+        #data = {'params': {'users': 100}}
+        data = {'params': 0}
+        headers={'content-type':'application/json'}
+        resultado = requests.get(url, params=0)
+        res = json.loads(resultado.content)
+        
+        #values =[]
+        #values.append(res)
+        
+        #for r in res:
+        #        values ={
+        #            "id":res["id"],
+        #            "name":res["name"]
+        #        }
+        values = []
+        for r in res:
+            values.append({"id":r["id"],"name":r["name"]})
+
+    #print(values)
+        
+        return json.dumps(values)
+        
+        
+        
+        
+        
+        #a = http.request.jsonrequest('https://jsonplaceholder.typicode.com/users')
+        #return json.dumps({'products':a})
+        
+        #return {
+        #           "type": "ir.actions.act_url",
+        #           "url": "https://odoo.com",
+        #           "target": "self",
+        #       }
+        
+    #    return "Hola Mundo"
+        
+        #req = requests.get('https://jsonplaceholder.typicode.com/users')
+        #req = requests.get('https://sebastian2161-ejercicio21-demo-web-3894096.dev.odoo.com/restfulapi/get-products')
+        #print(req.status_code)
+        #print(req.content)
+        #return req.content
+        #return req
+        #req.raise_for_status()
+        #parents_dict = req.json()
+        #return parents_dict
+        
+        #req = requests.get('https://jsonplaceholder.typicode.com/users', params=0)
+        #return req.json()
+        #req.raise_for_status()
+        #parents_dict = req.json()
+        #return parents_dict
+        
+        
+        
+        
+    # End-Point crear un producto nuevo    
+    #@http.route('/restfulapi/crear_product/<string:prod>', auth='none', website=True)
+    #def crear_product(self, prod, **kw):
+        #xmlrpclib = xmlrpc.client
+        
+    #    url = 'https://sebastian2161-ejercicio21-rama-v12-3748221.dev.odoo.com'
+    #    db = 'sebastian2161-ejercicio21-rama-v12-3748221'
+    #    username = 'admin'
+    #    password = 'admin'
+    
+    #    models = client.ServerProxy('{}/xmlrpc/2/object'.format(url))
+    #    common = client.ServerProxy('{}/xmlrpc/2/common'.format(url))
+        #print(common.version())
+        #return 'Hola Mundo'
+        
+        #try:
+    #    uid = common.authenticate(db, username, password, {})
+        #id1 = models.execute_kw(db, uid, password, 'product.template', 'create', [{'name': prod,}])
+    #    id2 = models.execute_kw(db, uid, password, 'product.product', 'create', [{'default_code': prod,'categ_id':8}])
+        
+        
+    #    return json.dumps({'products.product':id2, 'mensaje': 'Registro creado'})
+        #except:
+        #    return json.dumps({'Error':'Invalid Request'})
