2a7c33b24671539f34721edee2daf727b084079b
sebastian2161 <sebastiancoll25@gmail.com>
2022-03-11 14:30:47 +0000

    work58

--- odoo_academy/models/.ipynb_checkpoints/pos_payment_method-checkpoint.py
+++ odoo_academy/models/.ipynb_checkpoints/pos_payment_method-checkpoint.py
@@ -5,6 +5,6 @@ from odoo import models, fields, api
 class PosPaymentMethod01(models.Model):
     _inherit = 'pos.payment.method'
     
-    importe_tipo_metodo = fields.Float(string='importe tipo metodo', default=0.00, required=True, help="Total tipos de metodo")
+    importe_tipo_metodo = fields.Float(string='payment type amount', default=0.00, required=True, help="Total tipos de metodo")
     
     
--- odoo_academy/models/pos_payment.py
+++ odoo_academy/models/pos_payment.py
@@ -11,7 +11,7 @@ class PosPayment01(models.Model):
     #payment_method_id01 = fields.Many2one(comodel_name='pos.payment.method', string='importe_tipo_metodo', required=True)
     #tipo_pago01 = fields.Many2one(string='tipo_pago01',related='payment_method_id01.importe_tipo_metodo')
     
-    importe_tipo_metodo = fields.Float(string='importe tipo metodo', related='payment_method_id.importe_tipo_metodo', store=True)
+    importe_tipo_metodo = fields.Float(string='payment type amount', related='payment_method_id.importe_tipo_metodo', store=True)
     #importe_prueba01 = fields.Monetary(string='importe 01', default=1, required=True,oldname='importe_tipo_metodo', help="Importe 01")
     
     #campo01 = fields.Char(string ='campo01', default='t', required = True)
