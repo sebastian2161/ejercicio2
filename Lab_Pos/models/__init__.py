8be40cc72a0463c5a2b2c6259cc4170f941e658b
sebastian2161 <sebastiancoll25@gmail.com>
2022-03-11 20:22:11 +0000

    work04

--- Lab_Pos/models/__init__.py
+++ Lab_Pos/models/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from . import course
-from . import session
+#from . import course
+#from . import session
 from . import pos_payment_method_inherit
 from . import pos_payment_inherit
